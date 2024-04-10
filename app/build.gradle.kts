41620d5ffba3c0208330a959e73f7495e885a397
Juan Francisco Antonaya Lópe <juanfran.antonaya@escuelaestech.com>
2024-04-10 09:31:36 +0200

    añadimos AmiiboService

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,7 @@ dependencies {
     implementation ("androidx.activity:activity-ktx:1.2.2")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
     implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
---
+++ app/src/main/java/com/juanfra/ddapp/model/retrofit/AmiiboService.kt
@@ -0,0 +1,15 @@
+package com.juanfra.ddapp.model.retrofit
+
+import com.juanfra.ddapp.model.data.gameserieinfo.Amiibos
+import com.juanfra.ddapp.model.data.gameserieinfo.GameSeries
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface AmiiboService {
+    @GET("api/gameseries/")
+    suspend fun obtenerGameSeries() : Response<GameSeries>
+
+    @GET("api/amiibo/?gameseries=0x04f&showgames&showusage")
+    suspend fun obtenerListaAmiibos() : Response<Amiibos>
+
+}
